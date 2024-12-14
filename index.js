9a14c3f4aba5fb269d3c1a97ad83d9c538ab2798
Simileoluwa Oludare <25513055+simileoluwa@users.noreply.github.com>
2024-12-14 09:17:57 +0100

    learning the query selector

---
+++ index.js
@@ -0,0 +1,11 @@
+// "use strict";
+// const jahElo = document.getElementsByClassName("welcome");
+// jahElo.style.color = "red";
+
+// const jahElo = document.getElementsByClassName("welcome");
+// for (let element of jahElo) {
+//   element.style.color = "orange";
+// }
+
+const chosen = document.querySelector(".correct");
+console.log(chosen);
