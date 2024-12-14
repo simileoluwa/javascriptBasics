b6b829a4ed696d8295ba6d1b217efc72ff14b712
Simileoluwa Oludare <25513055+simileoluwa@users.noreply.github.com>
2024-12-14 09:16:57 +0100

    started the vowel counting strings

---
+++ countVowels.js
@@ -0,0 +1,8 @@
+let theVowels = ["a", "e", "i", "o", "u"];
+
+const countVowels = (counterAct) => {
+  let result = theVowels.length;
+  return result;
+};
+
+console.log(countVowels("heartingfriend"));
