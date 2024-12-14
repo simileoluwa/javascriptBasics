99e3d48a9e60f5cd4cde0a2bec87072d4fe3edc1
Simileoluwa Oludare <25513055+simileoluwa@users.noreply.github.com>
2024-12-14 10:26:38 +0100

    learnt how to use innerText and innerHtml

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-// "use strict";
+"use strict";
 // const jahElo = document.getElementsByClassName("welcome");
 // jahElo.style.color = "red";
 
 // const jahElo = document.getElementsByClassName("welcome");
 // for (let element of jahElo) {
-//   element.style.color = "orange";
+//   element.style.color = "red";
 // }
 
-const chosen = document.querySelector(".correct");
-console.log(chosen);
+// const lecture = document.querySelector(".welcome");
+
+// console.log(lecture.innerHTML);
+// console.log(lecture.innerText);
+// console.log(lecture.textContent);
+
+const features = document.querySelectorAll("p");
+// console.log(features);
+
+features.forEach((feature) => {
+  feature.style.color = "orange";
+  feature.innerText += " desire";
+  console.log(feature.innerText);
+});
