2062b29121a400104f9dd5b55bcef443b15a1e76
Simileoluwa Oludare <25513055+simileoluwa@users.noreply.github.com>
2024-12-14 16:09:24 +0100

    Learnt JS set and get attributes

--- index.js
+++ index.js
@@ -26,15 +26,25 @@
 // container.innerHTML += "<aside>This is the side</aside>";
 // console.log(container.innerHTML);
 
-const nameArr = [
-  "Simileoluwa Emmanuel",
-  "Yetunde Adejoro",
-  "Busayo Ebber",
-  "Peter Obi",
-  "Prof Seyi Akinde",
-];
+// const nameArr = [
+//   "Simileoluwa Emmanuel",
+//   "Yetunde Adejoro",
+//   "Busayo Ebber",
+//   "Peter Obi",
+//   "Prof Seyi Akinde",
+// ];
 
-const names = document.querySelector(".container");
-nameArr.forEach((name) => {
-  names.innerHTML += `<li>${name}</li>`;
-});
+// const names = document.querySelector(".container");
+// nameArr.forEach((name) => {
+//   names.innerHTML += `<li>${name}</li>`;
+// });
+
+// const getLink = document.querySelector("a");
+// console.log(getLink.getAttribute("href"));
+// getLink.setAttribute("href", "http://www.google.com");
+// console.log(getLink.getAttribute("href"));
+
+// const mfmSchools = document.querySelector(".mfmschools");
+// mfmSchools.setAttribute("class", "mainschools");
+// mfmSchools.setAttribute("style", "color:orange");
+// // console.log(mfmSchools.getAttribute("class"));
