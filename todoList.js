25c066309cb1fd15e105b62c08f15cd35fad8186
Simileoluwa Oludare <25513055+simileoluwa@users.noreply.github.com>
2025-01-09 16:11:48 +0100

    simple updates

--- todoList.js
+++ todoList.js
@@ -1,11 +1,23 @@
 // alert("I dey work");
 
+// const todoList = document.querySelector(".todoList");
+// const todoBtn = document.querySelector(".todoBtn");
+// const li = document.querySelector("li");
+
+// todoBtn.addEventListener("click", () => {
+//   const newLi = document.createElement("li");
+//   newLi.innerText = "This is the new List";
+//   todoList.appendChild(newLi);
+// });
+
 const todoList = document.querySelector(".todoList");
 const todoBtn = document.querySelector(".todoBtn");
-const li = document.querySelector("li");
+const mainList = document.querySelector("li");
 
-todoBtn.addEventListener("click", () => {
-  const newLi = document.createElement("li");
-  newLi.innerText = "This is the new List";
-  todoList.appendChild(newLi);
+todoBtn.addEventListener("click", () =>{
+  const newList = document.createElement("li");
+  newList.innerText = "Just trying new things";
+  newList.style.backgroundColor ="red";
+  todoList.appendChild(newList);
+
 });
